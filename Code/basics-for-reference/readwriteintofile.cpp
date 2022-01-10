7b07a0acb35e02b5b27626545b15f94a2933e218
acse-ns1321 <88569855+acse-ns1321@users.noreply.github.com>
2022-01-10 14:56:44 +0000

    Read Write Program

---
+++ Code/basics-for-reference/readwriteintofile.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+// include header fstream
+
+
+int main(){
+    
+    // create fstream variable named "file"
+    std::fstream file;
+
+
+    // //  WRITE MODE
+    // file.open("newTextFile.txt", std::ios::out); 
+    // // if file is open write some text into it
+    // if(file.is_open()){
+    //     file<< " Hi! I'm Niranjana. This is my first file!\n";
+    //     // close file
+    //     file.close();
+    // }
+
+    // // APPEND MODE
+    // file.open("newTextFile.txt", std::ios::app); 
+    // // if file is open APPEND some text into it
+    // if(file.is_open()){
+    //     file<< " Hi! I'm back\n";
+    //     // close file
+    //     file.close();
+    // }
+
+    // READ MODE
+    file.open("newTextFile.txt", std::ios::in); 
+    // If file is open, read the text in the file and write it out
+    if(file.is_open()){ 
+        // Method: Read first line - store it in a variable - write it out : repeat using while loop
+        // Note include the string library
+
+        std::string line;
+        while(getline(file, line)){// getline function takes file name and varible name as input
+        std::cout<<line<<std::endl; // print out each line as it is read
+        }
+        // Close file
+        file.close();
+    }
+}
---
+++ Code/basics-for-reference/tempCodeRunnerFile.cpp
@@ -0,0 +1,3 @@
+s::out); 
+    // // if file is open write some text into it
+    // if(file.is_open()){
